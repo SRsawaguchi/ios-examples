6906529e690f81095cc20a027f233a8edfe7c741
SRsawaguchi <naoyasawaguchi978@gmail.com>
2019-06-15 12:01:18 +0900

    initial commit

---
+++ SaveTextFileExample/SaveTextFileExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveTextFileExample
+//
+//  Created by Naoya Sawaguchi on 2019/06/15.
+//  Copyright © 2019 Naoya Sawaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
