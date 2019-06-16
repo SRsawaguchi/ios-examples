6407eda6c5e9424badfa3c033b665d3dc6e20243
SRsawaguchi <naoyasawaguchi978@gmail.com>
2019-06-16 16:30:59 +0900

    add view controller

--- SaveTextFileExample/SaveTextFileExample/ViewController.swift
+++ SaveTextFileExample/SaveTextFileExample/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    private var fileUrl: URL {
+        let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        return dir.appendingPathComponent("sample.txt")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        messageLabel.text = ""
     }
 
-
+    // TextViewnにファイルに保存したデータを表示する。
+    @IBAction func didTapLoadButton(_ sender: UIButton) {
+        do {
+            let text = try String(contentsOf: fileUrl)
+            textView.text = text
+        } catch {
+            messageLabel.text = "ファイルのロードに失敗しました。"
+            print("Error: \(error)")
+        }
+        
+    }
+    
+    // 入力されたテキストをファイルに保存する。
+    @IBAction func didTapSaveButton(_ sender: UIButton) {
+        guard let text = textView.text else { return }
+    
+        if FileManager.default.fileExists(atPath: fileUrl.path) {
+            // ファイルに書き込み
+            do {
+                try text.write(to: fileUrl, atomically: false, encoding: .utf8)
+                messageLabel.text = "ファイルに保存しました。"
+            } catch {
+                print("Error: \(error)")
+            }
+        } else {
+            // ファイルが存在しないため、新規に作成する。
+            if FileManager.default.createFile(
+                atPath: fileUrl.path,
+                contents: text.data(using: .utf8),
+                attributes: nil
+                ) {
+                messageLabel.text = "ファイルを新規作成しました。"
+            } else {
+                messageLabel.text = "ファイルの新規作成に失敗しました。"
+            }
+        }
+    }
 }
 
